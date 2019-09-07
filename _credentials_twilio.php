f15e507ccec0573299973f2f1c5b097783d30b13
JP-DEV\sjpad <sjpadgett@gmail.com>
2019-09-07 17:39:57 -0400

    Init oeFax

---
+++ _credentials_twilio.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+    'username'      => 'AC848bd81c1838dbc4d40e8a736ab2f901',
+    'extension'     => '+18136423974',
+    'password'      => '7b1a13b75e23dfcc4463b924a42503e2',
+    'appKey'        => 'SKa98a0e660c2121410adfe7a4586639aa',
+    'appSecret'     => 'S7Y7P8ay4Eg68buio4JUmoqpJ0nnVSML',
+    'server'        => '',
+    'portal'        => '',
+    'smsNumber'     => '+18136423974',
+    'production'    => '',
+    'redirect_url'  => '',
+    'smsHours'      => "50",
+    'smsMessage'    => "A courtesy reminder for ***NAME*** \r\nFor the appointment scheduled on: ***DATE*** At: ***STARTTIME*** Until: ***ENDTIME*** \r\nWith: ***PROVIDER*** Of: ***ORG***\r\nPlease call if unable to attend.",
+);
