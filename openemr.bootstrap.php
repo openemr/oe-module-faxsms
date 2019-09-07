11ed37a4437b5f51460d6ca4506b73324d88ee08
Stephen Nielson <stephen@nielson.org>
2019-09-07 19:08:56 -0400

    PatientReport btns & js output rendering.
    Added the event listener code to the patient report btns and js output rendering.

--- openemr.bootstrap.php
+++ openemr.bootstrap.php
@@ -35,3 +35,50 @@ function oe_module_faxsms_add_menu_item(MenuEvent $event) {
  * @global $module @see ModulesApplication::loadCustomModule
  */
 $eventDispatcher->addListener(MenuEvent::MENU_UPDATE, 'oe_module_faxsms_add_menu_item');
+
+
+function oe_module_faxsms_patient_report_render_action_buttons(Event $event) {
+?>
+<button type="button" class="genfax btn btn-default btn-send-msg btn-sm" value="<?php echo xla('Send Fax'); ?>" ><?php echo xlt('Send Fax'); ?></button><span id="waitplace"></span>
+<input type='hidden' name='fax' value='0'>
+<?php
+}
+$eventDispatcher->addListener(OpenEMR\Events\PatientReport::ACTIONS_RENDER_POST, 'oe_module_faxsms_patient_report_render_action_buttons');
+
+function oe_module_faxsms_patient_report_render_javascript_post_load(Event $event) {
+?>
+	function cleanUp(){
+	    $("#wait").remove();
+	}
+	function getFaxContent() {
+	    top.restoreSession();
+	    document.report_form.fax.value = 1;
+	    let url = 'custom_report.php';
+	    let wait = '<span id="wait"><?php echo xlt("Building Document .. ");?><i class="fa fa-cog fa-spin fa-2x"></i></span>';
+	    $("#waitplace").append(wait);
+	    $.ajax({
+		type: "POST",
+		url: url,
+		data: $("#report_form").serialize(),
+		success: function (content) {
+		    document.report_form.fax.value = 0;
+		    let btnClose = <?php echo xlj("Cancel"); ?>;
+		    let title = <?php echo xlj("Send To Contact"); ?>;
+		    let url = top.webroot_url + '/interface/modules/custom_modules/oe-module-faxsms/contact.php?isContent=0&file=' + content;
+		    dlgopen(url, '', 'modal-sm', 525, '', title, {
+			buttons: [
+			    {text: btnClose, close: true, style: 'default btn-xs'}
+			],
+			onClosed: 'cleanUp'
+		    });
+		    return false;
+		}
+	    }).always(function () {
+		$("#wait").remove();
+	    });
+	    return false;
+	}
+	$(".genfax").click(function() {getFaxContent();});
+<?php
+}
+$eventDispatcher->addListener(OpenEMR\Events\PatientReport::JAVASCRIPT_READY_POST, 'oe_module_faxsms_patient_report_render_javascript_post_load');
